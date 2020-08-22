12cead9b06bd67b2c5b7f16c2aa1e2104ec890ef
faqeel <contact@fares.ws>
2020-08-22 17:08:38 +0300

    feat: install service workers

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,24 +2,26 @@ const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const WorkboxPlugin = require('workbox-webpack-plugin');
 
 module.exports = merge(common, {
-  mode: 'production',
-  module: {
-    rules: [
-      {
-        test: /\.s[ac]ss$/i,
-        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
-      },
-    ],
-  },
-  plugins: [
-    new MiniCssExtractPlugin({ filename: '[name].css' }),
-    new CleanWebpackPlugin({
-      dry: true,
-      verbose: true,
-      cleanStaleWebpackAssets: true,
-      protectWebpackAssets: false,
-    }),
-  ],
+	mode: 'production',
+	module: {
+		rules: [
+			{
+				test: /\.s[ac]ss$/i,
+				use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+			},
+		],
+	},
+	plugins: [
+		new MiniCssExtractPlugin({ filename: '[name].css' }),
+		new CleanWebpackPlugin({
+			dry: true,
+			verbose: true,
+			cleanStaleWebpackAssets: true,
+			protectWebpackAssets: false,
+		}),
+		new WorkboxPlugin.GenerateSW(),
+	],
 });
