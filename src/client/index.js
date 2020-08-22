e660a64e0849940c8dfaeeba9c59cd6737a53b95
faqeel <contact@fares.ws>
2020-08-22 16:29:55 +0300

    refactor: improve the structure of frontend

--- src/client/index.js
+++ src/client/index.js
@@ -1,18 +1,17 @@
 import './styles/style.scss';
 
 // Dark/Light Mode Switch
-const theme = document.querySelector('.theme');
-theme.addEventListener('click', () => {
-  theme.classList.toggle('active');
-  alert('Sorry, this functionality is not implemented yet🏃‍♂️');
+document.querySelector('.theme-switch').addEventListener('click', () => {
+	document.querySelector('.theme-switch__icon').classList.toggle('theme-switch__icon--active');
+	alert('Sorry, this functionality is not implemented yet🏃‍♂️');
 });
 
 // Enable forecast element to scroll horizontally
-const forecast = document.querySelector('.forecast');
+const forecast = document.querySelector('.result__forecast');
 forecast.addEventListener('wheel', (e) => {
-  if (e.deltaY > 0) {
-    forecast.scrollLeft += 50;
-  } else {
-    forecast.scrollLeft -= 50;
-  }
+	if (e.deltaY > 0) {
+		forecast.scrollLeft += 50;
+	} else {
+		forecast.scrollLeft -= 50;
+	}
 });
