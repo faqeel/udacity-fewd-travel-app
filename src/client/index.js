135b9a7abaaf9fc7a5b2c0dc96fd8887a0f0780c
faqeel <contact@fares.ws>
2020-08-24 10:59:28 +0300

    feat: add DOMContentLoaded event

--- src/client/index.js
+++ src/client/index.js
@@ -2,22 +2,24 @@ import { handleSubmit, handleSaveTrip } from './js/app';
 
 import './styles/style.scss';
 
-document.getElementById('search_btn').addEventListener('click', handleSubmit);
+document.addEventListener('DOMContentLoaded', () => {
+	document.getElementById('search_btn').addEventListener('click', handleSubmit);
 
-document.getElementById('save_trip').addEventListener('click', handleSaveTrip);
+	document.getElementById('save_trip').addEventListener('click', handleSaveTrip);
 
-// Dark/Light Mode Switch
-document.querySelector('.theme-switch').addEventListener('click', () => {
-	document.querySelector('.theme-switch__icon').classList.toggle('theme-switch__icon--active');
-	alert('Sorry, this functionality is not implemented yet🏃‍♂️');
-});
+	// Dark/Light Mode Switch
+	document.querySelector('.theme-switch').addEventListener('click', () => {
+		document.querySelector('.theme-switch__icon').classList.toggle('theme-switch__icon--active');
+		alert('Sorry, this functionality is not implemented yet🏃‍♂️');
+	});
 
-// Enable forecast element to scroll horizontally
-const forecast = document.querySelector('.result__forecast');
-forecast.addEventListener('wheel', (e) => {
-	if (e.deltaY > 0) {
-		forecast.scrollLeft += 50;
-	} else {
-		forecast.scrollLeft -= 50;
-	}
+	// Enable forecast element to scroll horizontally
+	const forecast = document.querySelector('.result__forecast');
+	forecast.addEventListener('wheel', (e) => {
+		if (e.deltaY > 0) {
+			forecast.scrollLeft += 50;
+		} else {
+			forecast.scrollLeft -= 50;
+		}
+	});
 });
